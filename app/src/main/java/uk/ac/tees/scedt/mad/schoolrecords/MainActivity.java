06ee64839a2a61288e30c0b10eb00e219d28ac3c
w9541732 <w9541732@live.tees.ac.uk>
2022-11-18 13:41:45 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/scedt/mad/schoolrecords/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.scedt.mad.schoolrecords;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
